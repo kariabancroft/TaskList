887cfb2ba4cc2137f5ba2e047e41d79e611277d5
Kelly Souza <kellysouza99@gmail.com>
2017-04-05 14:16:48 -0700

    Testing branches

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -30,7 +30,7 @@ class TasksController < ApplicationController
 
 
   def update
-      @task=Task.find(params[:id])
+    @task=Task.find(params[:id])
     if @task.update(task_params)
       redirect_to tasks_path
     end
